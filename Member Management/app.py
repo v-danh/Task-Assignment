6cd11dd9a9d94d8bf6149e1bf6aee419a195ad4a
vdanh <vdanhvt2000@gmail.com>
2021-09-11 21:55:22 +0700

    first commit

---
+++ Member Management/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
